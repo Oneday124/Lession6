93594d725dab46104e3681b25b3eb364ddfce3f8
Nikolay <Onedaydejavu124@mail.ru>
2022-12-03 23:47:14 +0700

    Пример улучшения создания массива. Функции отступа от левого края и по высоте

---
+++ EX001/Program.cs
@@ -0,0 +1,15 @@
+﻿//
+
+string caption = "Intemsive C# Demo text";
+int screenWidtPosition = (Console.WindowWidth - caption.Length) / 2;
+int screenHeightPosition = Console.WindowHeight / 2;
+
+//DrawText(caption, screenWidtPosition, screenHeightPosition);
+
+//Как улучшить:
+
+//DrawText(
+//    text: caption,
+//    left: screenWidtPosition,
+//    top: screenHeightPosition
+//    );
